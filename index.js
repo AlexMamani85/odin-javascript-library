4419f04751de7ef4bfbcf949486a4fc2d079079b
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2024-03-24 18:23:37 -0500

    ADD ids, removeBook, cancelBtn, readInputLibrary

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ function addSeveralBooksToLibrary() {
     const book2 = new Book(myLibrary.length, "Harry Potter y la cámara secreta", "J. K. Rowling", 251, true);
     myLibrary.push(book2);
 
-    const book3 = new Book(myLibrary.length, "Harry Potter y el prisionero de Azkaban", "J. K. Rowling", 317);
+    const book3 = new Book(myLibrary.length, "Harry Potter y el prisionero de Azkaban", "J. K. Rowling", 317,false);
     myLibrary.push(book3);
 
-    const book4 = new Book(myLibrary.length, "Harry Potter y el cáliz de fuego", "J. K. Rowling", 636);   
+    const book4 = new Book(myLibrary.length, "Harry Potter y el cáliz de fuego", "J. K. Rowling", 636, false);   
     myLibrary.push(book4);
 
-    const book5 = new Book(myLibrary.length, "Harry Potter y la orden del Fénix", "J. K. Rowling", 766);
+    const book5 = new Book(myLibrary.length, "Harry Potter y la orden del Fénix", "J. K. Rowling", 766, false);
     myLibrary.push(book5);
 
     const book6 = new Book(myLibrary.length, "Harry Potter y el misterio del príncipe", "J. K. Rowling", 607, true);
@@ -38,6 +38,17 @@ function addSeveralBooksToLibrary() {
     const book7 = new Book(myLibrary.length, "Harry Potter y las reliquias de la muerte", "J. K. Rowling", 759, true);
     myLibrary.push(book7);
 
+}
+
+function removeBook(index, pags) {
+    myLibrary.forEach((el, i) => {
+        if (el.id == index) {
+            myLibrary.splice(i, 1);
+            return;
+        }
+    })
+    //myLibrary.splice(index, 1);
+    displayMyLibraryHTML();
 }
 
 
@@ -50,16 +61,18 @@ const array = document.querySelector("#array");
 
 const selectEl = newBookDialog.querySelector("select");
 
-titleInput=document.querySelector('#title');
-authorInput=document.querySelector('#author');
-pagesInput=document.querySelector('#pages');
-readInput=document.querySelector('#read');
+titleInput=newBookDialog.querySelector('#title');
+authorInput=newBookDialog.querySelector('#author');
+pagesInput=newBookDialog.querySelector('#pages');
+readInput=newBookDialog.querySelector('#read');
 
 
 const confirmBtn = newBookDialog.querySelector("#confirmBtn");
+const cancelBtn = newBookDialog.querySelector("#cancelBtn");
 
 // "Show the dialog" button opens the <dialog> modally
 newBookBTN.addEventListener("click", () => {
+    confirmBtn.innerHTML="Confirm"
     newBookDialog.showModal();
 });
 
@@ -87,8 +100,15 @@ confirmBtn.addEventListener("click", (event) => {
   newBookDialog.close(selectEl.value); // Have to send the select box value here.
 });
 
+cancelBtn.addEventListener("click", (event) => {
 
+    titleInput.value="";
+    authorInput.value="";
+    pagesInput.value="";
+    readInput.checked=false;
+    newBookDialog.close();
 
+  });
 
 
 
@@ -125,12 +145,28 @@ function displayMyLibraryHTML(){
         let titleDiv=document.createElement("div");
         let authorDiv=document.createElement("div");
         let pagesDiv=document.createElement("div");
-        let readInput=document.createElement("input");
+        let readInputLibrary=document.createElement("input");
         let readInputLabel=document.createElement("label");
+
+
     
         let editBtn=document.createElement("button");
         let deleteBtn=document.createElement("button");
         editBtn.textContent="Edit";
+
+        editBtn.addEventListener("click", () => {
+            newBookDialog.showModal();
+            console.log(book);
+            confirmBtn.innerHTML = "Edit";
+            titleInput.value=`${book.title}`;
+            authorInput.value=`${book.author}`;
+            pagesInput.value=`${book.pages}`;
+            console.log(typeof book.read);
+            readInput.checked = book.read;
+            
+            //confirmBtn.value = "Confirm";
+        });
+
         deleteBtn.textContent="Delete";
         containerDiv.style="padding: 30px;width=50px; border: 5px solid red; display:flex; flex-direction:column; justify-content:space-evenly;"
         containerDiv.style="display: grid; grid-template-columns: repeat(3, 1fr); grid-gap: 10px;"
@@ -143,22 +179,28 @@ function displayMyLibraryHTML(){
         
         let id=Math.random().toString(36).substr(2, 9);
 
-        readInput.type="checkbox";
+        readInputLibrary.type="checkbox";
         readInputLabel.textContent="Read";
         readInputLabel.for= id;
         readInputLabel.style="display:inline-block;padding: 10px 0px;"
-        readInput.style="display:inline-block;"
-        readInput.id=id;
-        readInput.name="read";
+        readInputLibrary.style="display:inline-block;"
+        readInputLibrary.id=book.id;
+        readInputLibrary.name="read";
 
         
         containerDataDiv.appendChild(titleDiv);
         containerDataDiv.appendChild(authorDiv);
         containerDataDiv.appendChild(pagesDiv);
 
-        containerCheckBoxDiv.appendChild(readInput);
+
+        readInputLabel.appendChild(readInputLibrary);
         containerCheckBoxDiv.appendChild(readInputLabel);
         
+        deleteBtn.addEventListener("click", () => {
+            console.log(book.id);
+            removeBook(book.id, book.pages);
+            
+        });
 
         buttonContainerDiv.appendChild(editBtn);
         buttonContainerDiv.appendChild(deleteBtn);
@@ -196,7 +238,7 @@ function displayMyLibraryHTML(){
         pagesDivSpan.textContent=`${book.pages}`;
         pagesDiv.appendChild(pagesDivSpan);
 
-        readInput.checked=book.read;
+        readInputLibrary.checked=book.read;
 
         titleDiv.style="padding: 5px 0px;"
         authorDiv.style="padding: 5px 0px;"
